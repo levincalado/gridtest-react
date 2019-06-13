b5f7c033fdb9f1d92666d3a2ce074a6114a4b629
Levin Calado <levincalado@gmail.com>
2019-06-13 16:52:33 +0800

    App.js: grid events

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    activebox: null,
+    counter: 0,
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    selected: 0,
+  };
+
+  render() {
+    const { items, activebox, selected } = this.state;
+    return (
+      <div>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {items.map((element, i) => {
+            if ((i + 1) % 5 === 0) {
+              return (
+                <Fragment>
+                  <div
+                    style={{
+                      width: "19vw",
+                      height: "20vw",
+                      border: "1px solid #000000",
+                      boxSizing: "border-box"
+                    }}
+                    onClick={() =>
+                      this.setState({ 
+                        activebox: Math.ceil((i + 1) / 5) * 5,
+                        selected: element
+                      })
+                    }
+                  >
+                    { element }
+                  </div>
+                  {activebox === i + 1 && (
+                    <div
+                      style={{
+                        width: "96%",
+                        height: "20vw",
+                        border: "1px solid #000000",
+                        boxSizing: "border-box"
+                      }}
+                    >
+                      { selected }
+                      <input
+                        style={{
+                          border: "1px solid #000000"
+                        }}
+                      />
+                    </div>
+                  )}
+                </Fragment>
+              );
+            } else {
+              return (
+                <div
+                  style={{
+                    width: "19vw",
+                    height: "20vw",
+                    border: "1px solid #000000",
+                    boxSizing: "border-box"
+                  }}
+                  onClick={() =>
+                    this.setState({ 
+                      activebox: Math.ceil((i + 1) / 5) * 5,
+                      selected: element
+                    })
+                  }
+                >
+                  {element}
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
